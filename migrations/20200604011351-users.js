791696cba73e66969bd91366c3c899fa0e620cf5
André Oliveira <andreluisoliveira013@gmail.com>
2020-06-03 22:20:32 -0300

    feat(api): Adding user model

---
+++ migrations/20200604011351-users.js
@@ -0,0 +1,52 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', {
+        id: {
+          type: Sequelize.INTEGER(11),
+          allowNull: false,
+          primaryKey: true,
+          comment: "null"
+        },
+        email: {
+          type: Sequelize.STRING(80),
+          allowNull: false,
+          comment: "null"
+        },
+        password: {
+          type: Sequelize.STRING(255),
+          allowNull: false,
+          comment: "null"
+        },
+        name: {
+          type: Sequelize.STRING(45),
+          allowNull: false,
+          defaultValue: 'username',
+          comment: "null"
+        },
+        avatar: {
+          type: Sequelize.STRING(80),
+          allowNull: false,
+          defaultValue: 'avatar.png',
+          comment: "null"
+        },
+        createdAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+          comment: "null"
+        },
+        updatedAt: {
+          type: Sequelize.DATE,
+          allowNull: false,
+          defaultValue: sequelize.literal('CURRENT_TIMESTAMP'),
+          comment: "null"
+        }
+      })
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('users');
+  }
+};
