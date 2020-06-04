1be00f63c0175a81eba3e0fc0c27116c96f7d246
Alessandro Menezes <alessandroadm@live.com>
2020-06-03 23:43:34 -0300

    created the edit route and edit method

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,13 +4,29 @@ const User = require('../models/User');
 module.exports = {
     save: async (req, res) => {
         try {
-            let { email, password, name, avatar} = req.body;
+            let { email, password, name, avatar } = req.body;
             const result = await User.create({
                 email, password, name, avatar
             });
             res.status(200).json({ result });
-        } catch(error) {
+        } catch (error) {
             res.status(401).json({ error });
         }
     },
-}
+
+    edit: async (req, res) => {
+        try {
+            let id = req.params.id;
+            let { email, password, name, avatar } = req.body;
+            let result = await User.update({ email, password, name, avatar },
+                {
+                where: {
+                    id
+                }
+            });
+            res.status(200).json({ result });
+        } catch (error) {
+            res.status(401).json({ error });
+        }
+    }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var userController = require('../controllers/userController')
-/* GET users listing. */
+var userController = require('../controllers/userController');
+
+//localhost:3000/users/save
 router.post('/save', userController.save);
+
+//localhost:3000/users/edit/1
+router.put('/edit/:id', userController.save);
 
 module.exports = router;
