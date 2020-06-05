b20a4f80ba348a15c302caab8c5cee3e5b1c08b5
Alessandro Menezes <alessandroadm@live.com>
2020-06-04 22:58:14 -0300

    create public route users

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var userController = require('../controllers/userController');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//localhost:3000/save
+router.post('/save', userController.save);
 
 module.exports = router;
