bb7f89537a46b4fe6daf3b4f39d5e4f461ae716a
Alessandro Menezes <alessandroadm@live.com>
2020-06-04 23:02:12 -0300

    fix userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ const { User } = require('../models');
 
 
 module.exports = {
+    
+    //-------------------------------------------------------
     save: async (req, res) => {
         try {
             let { email, password, name, avatar } = req.body;
@@ -15,31 +17,35 @@ module.exports = {
             res.status(401).json({ error });
         }
     },
-
+    
+    
+    //-------------------------------------------------------
     edit: async (req, res) => {
         try {
             let id = req.params.id;
             let { email, password, name, avatar } = req.body;
             let result = await User.update({ email, password, name, avatar },
                 {
-                where: {
-                    id
-                }
-            });
-            res.status(200).json({ result });
-        } catch (error) {
-            res.status(401).json({ error });
+                    where: {
+                        id
+                    }
+                });
+                res.status(200).json({ result });
+            } catch (error) {
+                res.status(401).json({ error });
+            }
+        },
+        
+        //-------------------------------------------------------
+        delete: async (req,res) => {
+            try {
+                let id = req.params.id;
+                let result = await User.destroy({
+                    where: { id }
+                });
+                res.status(200).json({ result });
+            } catch (error) {
+                res.status(401).json({ error });
+            }
         }
-    },
-    delete: async (req,res) => {
-        try {
-            let id = req.params.id;
-            let result = await User.destroy({
-                where: { id }
-            });
-            res.status(200).json({ result });
-        } catch (error) {
-            res.status(401).json({ error });
-        }
-    }
-};
+    };
