403233661b782a13d26b9e645bef064fdefd8a95
muriloignaccio <murilohsignacio@gmail.com>
2020-06-05 22:48:56 -0300

    feat(api): create route to find user by id

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,80 +3,96 @@ const sequelize = require('sequelize');
 const Op = sequelize.Op;
 
 module.exports = {
-    
-    //-------------------------------------------------------
-    save: async (req, res) => {
-        try {
-            let { email, password, name, avatar } = req.body;
-            const result = await User.create({
-                email, password, name, avatar
-            });
-            res.status(200).json({ result });
-        } catch (error) {
-            res.status(401).json({ error });
+  //-------------------------------------------------------
+  save: async (req, res) => {
+    try {
+      let { email, password, name, avatar } = req.body;
+      const result = await User.create({
+        email,
+        password,
+        name,
+        avatar,
+      });
+      res.status(200).json({ result });
+    } catch (error) {
+      res.status(401).json({ error });
+    }
+  },
+
+  //-------------------------------------------------------
+  edit: async (req, res) => {
+    try {
+      let id = req.params.id;
+      let { email, password, name, avatar } = req.body;
+      let result = await User.update(
+        { email, password, name, avatar },
+        {
+          where: {
+            id,
+          },
         }
-    },
-    
-    //-------------------------------------------------------
-    edit: async (req, res) => {
-        try {
-            let id = req.params.id;
-            let { email, password, name, avatar } = req.body;
-            let result = await User.update({ email, password, name, avatar },
-                {
-                    where: {
-                        id
-                    }
-                });
-                res.status(200).json({ result });
-            } catch (error) {
-                res.status(401).json({ error });
-            }
+      );
+      res.status(200).json({ result });
+    } catch (error) {
+      res.status(401).json({ error });
+    }
+  },
+
+  //-------------------------------------------------------
+  delete: async (req, res) => {
+    try {
+      let id = req.params.id;
+      let result = await User.destroy({
+        where: { id },
+      });
+      res.status(200).json({ result });
+    } catch (error) {
+      res.status(401).json({ error });
+    }
+  },
+
+  //-------------------------------------------------------
+  list: async (req, res) => {
+    try {
+      let users;
+      let { limit } = req.query;
+      if (limit) {
+        users = await User.findAll({ limit });
+      } else {
+        users = await User.findAll();
+      }
+      res.status(200).json({ users });
+    } catch (error) {
+      console.log(error);
+      res.status(401).json({ error });
+    }
+  },
+
+  //-------------------------------------------------------
+  findByName: async (req, res) => {
+    try {
+      let name = req.params.name;
+      let user = await User.findOne({
+        where: {
+          name: {
+            [Op.like]: `'%${name}%'`,
+          },
         },
-        
-        //-------------------------------------------------------
-        delete: async (req,res) => {
-            try {
-                let id = req.params.id;
-                let result = await User.destroy({
-                    where: { id }
-                });
-                res.status(200).json({ result });
-            } catch (error) {
-                res.status(401).json({ error });
-            }
-        },
-        
-        //-------------------------------------------------------
-        list: async (req, res) => {
-            try {
-                let users;
-                let { limit } = req.query.limit;
-                if (limit) {
-                    users = await User.findAll({ limit });
-                } else {
-                    users = await User.findAll();
-                }
-                res.status(200).json({ users });
-            } catch (error) {
-                res.status(401).json({ error });
-            }
-        },
-        
-        //-------------------------------------------------------
-        findByName: async (req, res) => {
-            try {
-                let name = req.params.name;
-                let user = await User.findOne({
-                    where: {
-                        name: {
-                        [Op.like]: `'%${name}%'`
-                        }
-                    }
-                });
-                res.status(200).json({ user });
-            } catch (error) {
-                res.status(401).json({ error });
-            }
-        }
-    };
+      });
+      res.status(200).json({ user });
+    } catch (error) {
+      res.status(401).json({ error });
+    }
+  },
+
+  //-------------------------------------------------------
+  findById: async (req, res) => {
+    try {
+      let { id } = req.params;
+      let user = await User.findByPk(id);
+      res.status(200).json(user);
+    } catch (error) {
+      res.status(401).json({ error });
+    }
+  },
+};
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var userController = require('../controllers/userController');
 
-
 //PRIVATE ROUTES
 //localhost:3000/users/save
 router.post('/save', userController.save);
@@ -16,7 +15,10 @@ router.delete('/delete/:id', userController.delete);
 //localhost:3000/users/list?limit=10
 router.get('/list', userController.list);
 
-//localhost:3000/users/Jane
+//localhost:3000/users/list/Jane
 router.get('/list/:name', userController.findByName);
+
+//localhost:3000/users/1
+router.get('/find/:id', userController.findById);
 
 module.exports = router;
