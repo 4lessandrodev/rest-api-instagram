f0b300dc5fe925724def035c95e73827d2333039
Alessandro Menezes <alessandroadm@live.com>
2020-06-08 22:58:03 -0300

    feat(api) Created delete post method

--- controllers/postController.js
+++ controllers/postController.js
@@ -23,62 +23,78 @@ module.exports = {
             let { limit = 10, page = 1 } = req.query;
             limit = parseInt(limit);
             page = parseInt(page - 1);
-            const { count:size, rows:posts } = await Post.findAndCountAll({
+            const { count: size, rows: posts } = await Post.findAndCountAll({
                 include: [
-                    { model: User, as:'user', attributes: ['id','name', 'avatar'] },
+                    { model: User, as: 'user', attributes: ['id', 'name', 'avatar'] },
                     {
                         model: Coment, as: 'coments', attributes: ['id', 'text'],
                         include: [
                             {
                                 model: User, as: 'user_coment', attributes: ['id', 'name', 'avatar']
                             }]
-                        }
-                    ],
-                    attributes: {exclude:['userId','createdAt','updatedAt']},
-                    limit,
-                    offset:limit*page
-                });
+                    }
+                ],
+                attributes: { exclude: ['userId', 'createdAt', 'updatedAt'] },
+                limit,
+                offset: limit * page
+            });
                 
-                res.status(200).json({ size, posts });
-            } catch (error) {
-                res.status(401).json({ error }); 
-            }
-        },
+            res.status(200).json({ size, posts });
+        } catch (error) {
+            res.status(401).json({ error });
+        }
+    },
+        
     // --------------------------------------------------
     findById: async (req, res) => {
-        const { id } = req.params
+        const { id } = req.params;
         try {
-            const post = await Post.findByPk(id ,{
+            const post = await Post.findByPk(id, {
                 include: [
-                    { model: User, as:'user', attributes: ['id','name', 'avatar'] },
+                    { model: User, as: 'user', attributes: ['id', 'name', 'avatar'] },
                     {
                         model: Coment, as: 'coments', attributes: ['id', 'text'],
                         include: [
                             {
                                 model: User, as: 'user_coment', attributes: ['id', 'name', 'avatar']
                             }]
-                        }
-                    ],
-                    attributes: {exclude:['userId','createdAt','updatedAt']}
-                });
-                
-                res.status(200).json({ post });
-            } catch (error) {
-                res.status(401).json({ error }); 
-            }
-        },
+                    }
+                ],
+                attributes: { exclude: ['userId', 'createdAt', 'updatedAt'] }
+            });
+            
+            res.status(200).json({ post });
+        } catch (error) {
+            res.status(401).json({ error });
+        }
+    },
+            
+    // --------------------------------------------------
     edit: async (req, res) => {
         try {
-            const { id } = req.params
-            const { text } = req.body
+            const { id } = req.params;
+            const { text } = req.body;
             const post = await Post.update(
                 { text }, {
-                    where: id
-                })
-            res.status(200).json({ post })
-
+                where: id
+            });
+            res.status(200).json({ post }); 
         } catch (error) {
-            res.status(401).json({ error})
+            res.status(401).json({ error });
+        }
+    },
+            
+    // --------------------------------------------------
+    delete: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const post = await Post.destroy({
+                where: { id }
+            });
+            res.status(200).json({ post });
+        } catch (error) {
+            console.log(error);
+            res.status(401).json({ error });
         }
     }
-    }
+};
--- routes/posts.js
+++ routes/posts.js
@@ -10,10 +10,13 @@ router.post('/save', postController.save);
 router.get('/list', postController.list);
 
 //localhost:3000/posts/find/1
-router.get('/find/:id', postController.findById)
+router.get('/find/:id', postController.findById);
 
 //localhost:3000/posts/edit/1
-router.post('/edit/:id', postController.edit)
+router.post('/edit/:id', postController.edit);
+
+//localhost:3000/posts/delete/1
+router.delete('/delete/:id', postController.delete);
 
 
 module.exports = router;
